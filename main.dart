b51a346728539b7759993f37fe96be442e064926
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2022-07-06 22:04:45 -0300

    alteraçao main.dart

--- main.dart
+++ main.dart
@@ -2,9 +2,9 @@ import 'hospital.dart';
 import 'unidade.dart';
 
 void main() {
-  // Unidade unidadeABC = Unidade('ABC');
-  // unidadeABC.nome = 'a';
-  // print(unidadeABC.nome);
+  Unidade unidadeABC = Unidade('ABC');
+  unidadeABC.nome = 'a';
+  print(unidadeABC.nome);
 
   Unidade unidadeSantoAmaro = Hospital('Santo Amaro');
   unidadeSantoAmaro.nome = 'ABCD';
