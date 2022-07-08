3d2eb804c7124f16c667ce41a6c860787fd7919d
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2022-07-07 22:07:46 -0300

    final/copyWith

--- main.dart
+++ main.dart
@@ -10,6 +10,10 @@ void main() {
   LojaDoRonaldo loja2 = LojaDoRonaldo('RonaldoLolo', 'ZL', Motoca('Clebinho'));
   loja2.delivery();
 
+  loja1 = loja1.copyWith('Ronaldo do Whisky');
+  loja1.delivery();
+  // CopyWith Patter before loja1 != after loja1
+  // before loja1 == after loja1 porem before loja1.nome != after loja1.nome
   // Hospital hospital = Hospital();
   // IUnidade posto = Posto();
   // Paciente usuario = Paciente('avenida', -50, hospital);
--- paciente.dart
+++ paciente.dart
@@ -2,14 +2,22 @@ import 'hospital.dart';
 import 'unidade_interface.dart';
 
 class LojaDoRonaldo {
-  String nome;
-  String bairro;
+  // final torna imutavel
+  final String nome;
+  String _bairro;
   IEntregador entregador;
+
   //construtor
   LojaDoRonaldo(
-      String this.nome, String this.bairro, IEntregador this.entregador);
+      String this.nome, String this._bairro, IEntregador this.entregador);
+
+  String get bairro => _bairro;
 
   void delivery() {
-    entregador.entregar(this, this.bairro);
+    entregador.entregar(this, this._bairro);
+  }
+
+  LojaDoRonaldo copyWith(String nome) {
+    return LojaDoRonaldo(nome, this._bairro, this.entregador);
   }
 }
